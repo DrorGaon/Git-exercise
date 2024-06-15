f334026829e57e1038ce285deca443061d3ca501
DrorGaon <drordtuii@gmail.com>
2024-06-15 16:08:38 +0300

    Add ball size limit and reset

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 function onBallClick(elBall){
     var ballSize = elBall.offsetWidth + 50
+    if (ballSize > 400) ballSize = 100
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
     elBall.innerText = ballSize
