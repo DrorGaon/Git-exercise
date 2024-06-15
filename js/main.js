dab880730c4213cc9409f6e3d8a40ced28affa16
DrorGaon <drordtuii@gmail.com>
2024-06-15 16:28:01 +0300

    Add random ball size increment

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 function onBallClick(elBall){
-    var ballSize = elBall.offsetWidth + 50
+    var ballSize = elBall.offsetWidth + getRandomIntInclusive(20, 60)
     if (ballSize > 400) ballSize = 100
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
